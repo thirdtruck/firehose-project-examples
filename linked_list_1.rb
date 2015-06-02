b276e53a1b468e5935562ccb80cd670678dedb82
James Holder <github.com@thirdtruck.org>
2015-06-01 20:10:44 -0400

    Add starting code for Linked List Problem #1.

---
+++ linked_list_1.rb
@@ -0,0 +1,32 @@
+# Classes
+
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+# Functions
+
+def print_values(list_node)
+  print "#{list_node.value} --> "
+  if list_node.next_node.nil?
+    print "nil\n"
+    return
+  else
+    print_values(list_node.next_node)
+  end
+end
+
+# Example Nodes
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+# Example Usage
+
+print_values(node3)
