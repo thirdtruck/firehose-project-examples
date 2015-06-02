64dec7ddb1e3122195e546788ad94c9703a8121f
James Holder <github.com@thirdtruck.org>
2015-06-01 21:46:07 -0400

    Add starting code for Linked List Problem #2.

---
+++ linked_list_2.rb
@@ -0,0 +1,44 @@
+# Classes
+
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+
+# Functions
+
+def print_values(list_node)
+  print "#{list_node.value} --> "
+  if list_node.next_node.nil?
+    print "nil\n"
+    return
+  else
+    print_values(list_node.next_node)
+  end
+end
+
+def reverse_list(list)
+  # CODE HERE
+end
+
+# Example Nodes
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+# Example Usage
+
+print_values(node3)
+
+# Linked List Problem #1
+
+revlist = reverse_list(node3)
+
+print_values(revlist)
+
