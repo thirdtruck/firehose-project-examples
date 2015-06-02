9d630f85059875f83a3ebe26b5c77e424428ad23
James Holder <github.com@thirdtruck.org>
2015-06-02 00:46:07 -0400

    Implement #is_infinite.

--- linked_list_bonus.rb
+++ linked_list_bonus.rb
@@ -1,3 +1,5 @@
+# Classes
+
 class LinkedListNode
   attr_accessor :value, :next_node
 
@@ -6,6 +8,8 @@ class LinkedListNode
     @next_node = next_node
   end
 end
+
+# Functions
 
 def print_values(list_node)
   print "#{list_node.value} --> "
@@ -17,10 +21,52 @@ def print_values(list_node)
   end
 end
 
+def is_infinite(list)
+  lam, mu = floyd(list)
+  return lam > 1
+end
+
+def f(node)
+  node.next_node if node
+end
+
+# For the sake of simplicity, we assume that we've been passed the first node in a list that has
+# at least two nodes (a prerequisite for a loop-capable list).
+def floyd(x0)
+  tortoise = f(x0)
+  hare = f(f(x0))
+
+  while tortoise != hare
+    tortoise = f(tortoise)
+    hare = f(f(hare))
+  end
+
+  mu = 0
+  tortoise = x0
+  while tortoise != hare
+    tortoise = f(tortoise)
+    hare = f(hare)
+    mu += 1
+  end
+
+  lam = 1
+  hare = f(tortoise)
+  while tortoise != hare
+    hare = f(hare)
+    lam += 1
+  end
+
+  return [lam, mu]
+end
+
+# Example Nodes
+
 node1 = LinkedListNode.new(37)
 node2 = LinkedListNode.new(99, node1)
 node3 = LinkedListNode.new(12, node2)
 node1.next_node = node3
+
+# Example Usage
 
 if is_infinite(node3)
   puts "Cannot print infinite lists."
